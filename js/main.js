21b25c5626a15989ee0e864423d44bdbacca9583
Roseblog19 <ak6566276@gmail.com>
2026-03-04 16:04:30 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ container.innerHTML += `
 
 <div class="card">
 
+<img src="${post.image}" class="card-img">
+
 <h3>${post.title}</h3>
 
 <p>${post.description}</p>
@@ -19,82 +21,9 @@ Read More
 </a>
 
 </div>
+
 `;
 
 });
 
 });
-document.getElementById("search").addEventListener("keyup", function(){
-
-let value = this.value.toLowerCase();
-
-let cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-let text = card.innerText.toLowerCase();
-
-if(text.includes(value)){
-card.style.display="block";
-}else{
-card.style.display="none";
-}
-
-});
-  document.getElementById("category").addEventListener("change", function(){
-
-let value = this.value;
-
-let cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-if(value === "all"){
-card.style.display="block";
-}else if(card.innerText.includes(value)){
-card.style.display="block";
-}else{
-card.style.display="none";
-}
-
-});
-
-});
-
-
-});
-const searchInput = document.getElementById("search");
-const categorySelect = document.getElementById("category");
-
-function filterPosts(){
-
-let searchValue = searchInput.value.toLowerCase();
-let categoryValue = categorySelect.value;
-
-let cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-let text = card.innerText.toLowerCase();
-
-let categoryMatch =
-categoryValue === "all" || text.includes(categoryValue.toLowerCase());
-
-let searchMatch = text.includes(searchValue);
-
-if(searchMatch && categoryMatch){
-card.style.display="block";
-}else{
-card.style.display="none";
-}
-
-});
-
-}
-
-searchInput.addEventListener("keyup", filterPosts);
-categorySelect.addEventListener("change", filterPosts);
-
-
-
-
