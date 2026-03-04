a390b446358e1140b2596b79a46526d3dbc91eeb
Roseblog19 <ak6566276@gmail.com>
2026-03-04 15:41:54 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,5 +63,38 @@ card.style.display="none";
 
 
 });
+const searchInput = document.getElementById("search");
+const categorySelect = document.getElementById("category");
+
+function filterPosts(){
+
+let searchValue = searchInput.value.toLowerCase();
+let categoryValue = categorySelect.value;
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+let text = card.innerText.toLowerCase();
+
+let categoryMatch =
+categoryValue === "all" || text.includes(categoryValue.toLowerCase());
+
+let searchMatch = text.includes(searchValue);
+
+if(searchMatch && categoryMatch){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+}
+
+searchInput.addEventListener("keyup", filterPosts);
+categorySelect.addEventListener("change", filterPosts);
+
+
 
 
